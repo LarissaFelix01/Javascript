411463fa439f9bd31ea6c14a64236b4a659de01c
larissa felix <larissa.felixx1@gmail.com>
2023-07-16 18:20:42 -0300

    fiz uma estrutura de json de pessoas

---
+++ aula06.js
@@ -0,0 +1,30 @@
+const pessoa = [
+    {
+    id: 1,
+    nome: 'rafael',
+    cpf: '709.684.965-32',
+    idade: 36,
+    peso: 190.0,
+    telefone: '(84)99456385',
+    endereco: {
+        rua: 'nestor gargaldo',
+        numero: 206,
+        cep: 59444445
+    }
+
+}, 
+    {
+    id: 2,
+    nome: 'larissa',
+    cpf: '',
+    idade: 36,
+    peso: 190.0,
+    telefone: '(84)99456385',
+    endereco: {
+        rua: 'nestor gargaldo',
+        numero: 206,
+        cep: 59444445
+    }
+
+}]
+console.log (pessoa[0])
