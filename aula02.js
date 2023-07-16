4115f3a27bf8a369cc521b9851714dffc0ec323e
larissa felix <larissa.felixx1@gmail.com>
2023-07-16 16:48:04 -0300

    fiz uma condicional na variavel idade

---
+++ aula02.js
@@ -0,0 +1,9 @@
+const nome = 'rafael'
+const idade = 16
+
+if (idade >18){
+    console.log(nome + ' é maior de idade, ele tem ' + idade + ' anos')
+}else{ 
+    console.log(nome + ' não é maior de idade, ele tem menos de 18 anos')
+}
+
