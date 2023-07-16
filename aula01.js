de95f3dd90badc6fce676d322a715cff13a4d742
larissa felix <larissa.felixx1@gmail.com>
2023-07-16 16:19:34 -0300

    aula foi sobre tipos de  variaveis js

---
+++ aula01.js
@@ -0,0 +1,7 @@
+const nome = 'Larissa'
+const idade = 21
+const peso = 65.8
+
+console.log('Nome: ',nome)
+console.log('Idade: ',idade + ' anos')
+console.log("Meu nome é " + nome + " tenho " + idade + " anos" )
